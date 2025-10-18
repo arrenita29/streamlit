45d8fb0d32ae53b177f2993d2600de78e6203050
Ken McGrady <ken.mcgrady@gmail.com>
2025-10-18 14:01:44 -0700

    Migrate getElements to the ElementsSetVisitor

--- frontend/lib/src/render-tree/BlockNode.ts
+++ frontend/lib/src/render-tree/BlockNode.ts
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-import { Block as BlockProto, Element } from "@streamlit/protobuf"
+import { Block as BlockProto } from "@streamlit/protobuf"
 
-import { isNullOrUndefined, notUndefined } from "~lib/util/utils"
+import { notUndefined } from "~lib/util/utils"
 
 import { AppNode } from "./AppNode.interface"
 import { AppNodeVisitor } from "./visitors/AppNodeVisitor.interface"
@@ -184,18 +184,6 @@ export class BlockNode implements AppNode {
       this.fragmentId,
       this.deltaMsgReceivedAt
     )
-  }
-
-  public getElements(elementSet?: Set<Element>): Set<Element> {
-    if (isNullOrUndefined(elementSet)) {
-      elementSet = new Set<Element>()
-    }
-
-    for (const child of this.children) {
-      child.getElements(elementSet)
-    }
-
-    return elementSet
   }
 
   public accept<T>(visitor: AppNodeVisitor<T>): T {
